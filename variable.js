ca34698068b9be5d68713570d815bf581e62f7a6
Himanshu Shekhar <131612304+Luna7282@users.noreply.github.com>
2024-07-13 12:48:41 +0530

    challenge Start

---
+++ variable.js
@@ -0,0 +1,37 @@
+
+function typ(ele){
+    console.log(typeof(ele));
+}
+
+// Activity 1
+var imp ;
+let tr;
+imp = 21
+tr = 'Ak'
+console.log(imp);
+console.log(tr);
+ // Activity 2 
+const fps = true
+console.log(fps);
+ // Activity 3
+  var a = 21
+ let b = 'Chai'
+ const yt = false
+ let root = Object.create({name : "IPS"})
+ let arr = Array[21,23,343]
+ typ(b)
+ typ(a)
+ typ(b)
+ typ(root)
+
+// Activity 4 
+let pos ;
+pos = 'ak'
+console.log(pos);
+pos = 31
+console.log(pos);
+
+// Activity 5
+const really = true
+// const really = false
+console.log(really);
