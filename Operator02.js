a5286619a614a1ccda8e7adfffa4f9c53370b2fa
Himanshu Shekhar <131612304+Luna7282@users.noreply.github.com>
2024-07-14 13:15:38 +0530

    2 challenge Complete

---
+++ Operator02.js
@@ -0,0 +1,53 @@
+// Activity 1
+
+   let a=20 , b=30
+   console.log("Sum of a+b =",a+b);
+   console.log("Sum of a-b =",a-b);
+   console.log("Sum of a*b =",a*b);
+   console.log("Sum of a/b =",a/b);
+   console.log("Sum of a//b =",a%b);
+
+  // Activity 2
+  var z = 23
+  console.log(z+=23); 
+  console.log(z-=20); 
+
+  // Activity 3
+  let Age_1=21, Age_2=17
+  if(Age_1>Age_2){
+    console.log("B1 is older than B2");
+  }
+  else{
+    onsole.log("B1 is younger than B2");
+  }
+  let marks1 = 98 , marks2 = 87
+     if(marks1>=marks2){
+        console.log("B1 scored Higher than B2");
+     }
+     else{
+        console.log("B2 scored Higher than B1");
+     }
+    
+     let v = 21
+     let c = '21' 
+     if(v===c){
+        console.log("Both are same ");
+     }
+     if(v==c){
+        console.log("Both are not same ");
+     }
+///  Activity 4
+ let f = true, y = null
+ if(f&&v === true){
+    console.log("Both are true");
+ }
+ if(f||v){
+    console.log("Both are not true");
+ }
+ if(f !=v){
+    console.log("Both are not true ...");
+ }
+
+ // Activity 5
+ let sub = (100>1000) ? "yes" : "No"
+ console.log(sub);
